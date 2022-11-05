07d98bfa670381546f5f7287b33b8609f64a1da1
Vyacheslav Soldatov <kibersoldier@gmail.com>
2022-11-05 14:22:31 +0400

    let's start doing business

---
+++ src/main/java/com/leaderhackdemo/servicerequests/ServicerequestsApplication.java
@@ -0,0 +1,13 @@
+package com.leaderhackdemo.servicerequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicerequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicerequestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leaderhackdemo/servicerequests/ServicerequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaderhackdemo.servicerequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicerequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
